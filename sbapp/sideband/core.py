1595afaca3895ccf7eaaed30748bde5b5cb34e11
Mark Qvist <mark@unsigned.io>
2022-10-02 23:05:06 +0200

    Added shader to wheel build

--- sbapp/sideband/core.py
+++ sbapp/sideband/core.py
@@ -906,7 +906,7 @@ class SidebandCore():
             self.service_thread.start()
         
     def __start_jobs_immediate(self):
-        self.reticulum = RNS.Reticulum(configdir=self.rns_configdir, loglevel=2)
+        self.reticulum = RNS.Reticulum(configdir=self.rns_configdir, loglevel=3)
 
         if RNS.vendor.platformutils.get_platform() == "android":
             if not self.reticulum.is_connected_to_shared_instance:
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://unsigned.io/sideband",
     packages=setuptools.find_packages(),
-    package_data={'': ['kivymd/fonts/*','kivymd/images/*']},
+    package_data={'': ['kivymd/fonts/*','kivymd/images/*','kivymd/data/glsl/elevation/*']},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
