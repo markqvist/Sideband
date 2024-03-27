48908842c6c98a9e9aa2c1c984dd76c66fca3300
Mark Qvist <mark@unsigned.io>
2024-03-27 15:31:38 +0100

    Fixed asset path on Windows

--- sbapp/sideband/core.py
+++ sbapp/sideband/core.py
@@ -155,6 +155,9 @@ class SidebandCore():
         elif RNS.vendor.platformutils.get_platform() == "linux":
             core_path          = os.path.abspath(__file__)
             self.asset_dir     = core_path.replace("/sideband/core.py", "/assets")
+        elif RNS.vendor.platformutils.is_windows():
+            core_path          = os.path.abspath(__file__)
+            self.asset_dir     = core_path.replace("\\sideband\\core.py", "\\assets")
         else:
             self.asset_dir     = plyer.storagepath.get_application_dir()+"/sbapp/assets"
 
