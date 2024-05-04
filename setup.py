70fb2170beb2b08a11ae80ff6138841f9318edc2
Mark Qvist <mark@unsigned.io>
2024-05-04 23:27:27 +0200

    Updated dependency version

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setuptools.setup(
             'sideband=sbapp:main.run',
         ]
     },
-    install_requires=["rns>=0.7.3", "lxmf>=0.4.3", "kivy>=2.3.0", "plyer", "pillow>=10.2.0", "qrcode", "materialyoucolor>=2.0.7"],
+    install_requires=["rns>=0.7.4", "lxmf>=0.4.3", "kivy>=2.3.0", "plyer", "pillow>=10.2.0", "qrcode", "materialyoucolor>=2.0.7"],
     extras_require={
         "macos": ["pyobjus"],
     },
