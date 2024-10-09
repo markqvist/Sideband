763db207701cfbf4506a6e9b28ac848bc273f5e0
Mark Qvist <mark@unsigned.io>
2024-10-09 20:10:24 +0200

    Updated version and dependencies

--- setup.py
+++ setup.py
@@ -99,8 +99,8 @@ setuptools.setup(
         ]
     },
     install_requires=[
-        "rns>=0.8.1",
-        "lxmf>=0.5.4",
+        "rns>=0.8.3",
+        "lxmf>=0.5.5",
         "kivy>=2.3.0",
         "pillow>=10.2.0",
         "qrcode",
