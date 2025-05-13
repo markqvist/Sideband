98c64acb3bf83e148fd0e9cde21fa9582bfb0562
Mark Qvist <mark@unsigned.io>
2025-05-13 18:13:01 +0200

    Updated version and RNS dependency version

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ setuptools.setup(
         ]
     },
     install_requires=[
-        "rns>=0.9.5",
+        "rns>=0.9.6",
         "lxmf>=0.7.0",
         "kivy>=2.3.0",
         "pillow>=10.2.0",
