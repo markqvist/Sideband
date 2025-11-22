6edc6e377193aaf69750849ff18343fca8728441
Mark Qvist <mark@unsigned.io>
2025-11-22 10:28:42 +0100

    Render toast component in-application on all platforms

--- sbapp/kivymd/toast/__init__.py
+++ sbapp/kivymd/toast/__init__.py
@@ -2,10 +2,14 @@ __all__ = ("toast",)
 
 from kivy.utils import platform
 
+use_native_toast = False
+
 if platform == "android":
-    try:
-        from .androidtoast import toast
-    except ModuleNotFoundError:
+    if use_native_toast:
+        try: from .androidtoast import toast
+        except ModuleNotFoundError: from .kivytoast import toast
+    else:
         from .kivytoast import toast
+
 else:
     from .kivytoast import toast
