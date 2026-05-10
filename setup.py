888ef4f4b02c666eef608deef6ede479c54bcb90
Mark Qvist <bc7291552be7a58f361522990465165c>
2026-05-10 17:26:20 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -114,8 +114,8 @@ setuptools.setup(
         ]
     },
     install_requires=[
-        "rns>=1.2.4",
-        "lxmf>=0.9.7",
+        "rns>=1.2.5",
+        "lxmf>=0.9.8",
         "lxst>=0.4.6",
         "kivy>=2.3.0",
         "numpy>=2.0.0",
